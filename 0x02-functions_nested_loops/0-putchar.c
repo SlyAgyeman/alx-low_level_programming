aa5bb88729c18e5cb04973a132b0629dee3b1023
Sylvia <abenasasu14@gmail.com>
2023-02-21 15:59:27 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,11 @@
  *
  * 	Return: Always 0 (Success)
  */
-void print_alphabet(void)'
+
+void print_alphabet(void) *
 {
 	char letter;
 	for {letter = 'a'; letter <= 'z'; letter++}
-		_putchar(letter);
+		putchar(letter);
 	_putchar('\n');
 }	

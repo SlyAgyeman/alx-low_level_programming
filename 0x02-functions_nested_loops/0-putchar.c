f57091807250725c25f9fd0b3704a2a5c3c6c4a1
Sylvia <abenasasu14@gmail.com>
2023-02-21 15:49:13 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * 	Return: Always 0 (Success)
+ */
+void print_alphabet(void)'
+{
+	char letter;
+	for {letter = 'a'; letter <= 'z'; letter++}
+		_putchar(letter);
+	_putchar('\n');
+}	

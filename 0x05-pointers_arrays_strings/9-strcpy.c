cf808b10147cbd560b6ef5545eaa537f3eec097d
Sylvia <abenasasu14@gmail.com>
2023-02-28 14:17:48 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int | = 0;
+	int x = 0;
+
+	while (*(src + |) != '\0')
+	{
+	        |++;
+	}
+	for ( ; x < | ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[|] = '\0';
+	return (dest);
+}
